49a83dc1f379c9bdce34c72f3c36afec898a4ba7
FlyZhao <46128378+zhaoZhuolin@users.noreply.github.com>
2024-02-19 09:20:31 +0800

    test

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ const myName: number = 1;
 const myName1: number = 1;
 const place: string = "beijing";
 const arr = [1, 2, 3];
+const arr2 = [1, 2, 3];
 for (let i = 0; i < arr.length; i++) {
   const element = arr[i];
   console.log(element);
