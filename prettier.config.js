d982896a63cf28be92f74a2734a9dc167d3a9c6d
FlyZhao <46128378+zhaoZhuolin@users.noreply.github.com>
2024-02-19 09:17:42 +0800

    test

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // 句末是否加分号
+  semi: true,
+  // 是否用单引号
+  singleQuote: false,
+  // tab健的空格数
+  tabWidth: 2,
+  //单行长度
+  printWidth: 80,
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-const myName:Number = 1;
-const myName1:Number = 1;
-
+const myName: number = 1;
+const myName1: number = 1;
+const place: string = "beijing";
+const arr = [1, 2, 3];
+for (let i = 0; i < arr.length; i++) {
+  const element = arr[i];
+  console.log(element);
+  console.log(123);
+}
+console.log(myName);
+console.log(myName1);
+console.log(place);
